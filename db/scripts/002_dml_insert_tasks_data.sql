1e8ee4790280caad815159660f0614d0ac928a11
v.krasov.ext <v.krasov@aleria.cloud>
2025-05-11 17:28:25 +0400

    edit pom.xml, update db config and edit hibernate.cfg.xml

---
+++ db/scripts/002_dml_insert_tasks_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks (name, description, created, completed) VALUES ('Item 1', 'do stuff', '2022-08-24T15:59:57', false);
+INSERT INTO tasks (name, description, created, completed) VALUES ('Item 2', 'do stuff', '2023-08-23T15:59:57', true);
+INSERT INTO tasks (name, description, created, completed) VALUES ('Item 3', 'do stuff', '2024-08-24T16:59:57', false);
