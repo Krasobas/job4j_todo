a09e611ffb624d4a58dc8f9dd78054c3d79f8597
v.krasov.ext <v.krasov@aleria.cloud>
2025-05-11 17:30:47 +0400

    edit model

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,21 @@
+package ru.job4j.todo.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @CreationTimestamp
+    private LocalDateTime created;
+    private Boolean completed = false;
+}
